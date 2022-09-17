32380db1c4fcd1dc6f68186c1b0548df31ec1489
raviroygit <ravi93448@yahoo.com>
2022-09-17 18:44:03 +0530

    hide humburger icon when large screen and drawer open

--- src/components/Custom/CustomHeader.js
+++ src/components/Custom/CustomHeader.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Appbar } from "react-native-paper";
+import { large, useBreakpoint } from '../../utils/Drawer/Breakpoint';
+
+const showDrawerToggleForBreakpoint = breakpoint => breakpoint !== large;
 
 const AppBar = ({ navigation, options, back }) => {
+    const breakpoint = useBreakpoint();
+    const showDrawerToggle = showDrawerToggleForBreakpoint(breakpoint);
 
     return (
         <>
             <Appbar.Header>
-                <Appbar.Action
+                {/* <Appbar.Action
                     accessibilityLabel="Menu"
                     icon="menu"
                     onPress={navigation.toggleDrawer}
-                />
+                /> */}
+
+                {showDrawerToggle && (
+                    <Appbar.Action
+                        accessibilityLabel="Menu"
+                        icon="menu"
+                        onPress={navigation.toggleDrawer}
+                    />
+                )}
+
                 {back ? <Appbar.BackAction
                     onPress={navigation.goBack}
                     accessibilityLabel="Back"
