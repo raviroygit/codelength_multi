686704a07eb2c6c6765b136507f21cd64ae8076e
raviroygit <ravi93448@yahoo.com>
2022-09-17 18:29:14 +0530

    breakpoint added

--- src/components/Navigation/NavigationContent.js
+++ src/components/Navigation/NavigationContent.js
@@ -3,21 +3,27 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomescreenHeader from '../Screen/HomeScreen/HomescreenHeader';
 import CustomNavigationDrawer from '../Custom/CustomNavigationDrawer';
 import BlogHeader from '../Screen/Blogs/BlogHeader';
-
+import { large,useBreakpoint } from '../../utils/Drawer/Breakpoint';
 
 const Drawer = createDrawerNavigator();
 
 const NavigationContent = () => {
+    const getDrawerTypeForBreakpoint = breakpoint =>
+    breakpoint === large ? 'permanent' : 'back';
+
+    const breakpoint = useBreakpoint();// called custom hooks for change imediatly when screen wisth changes
+    const drawerTypeForBreakpoint = getDrawerTypeForBreakpoint(breakpoint);
 
     return (
         <Drawer.Navigator
-            // screenOptions={{
-            //     // drawerStyle: {
-            //     //   backgroundColor: "white",
-            //     //   zIndex: 100,
-            //     // },
-            //     // drawerPosition: "right",
-            // }}
+            screenOptions={{
+                // drawerStyle: {
+                //   backgroundColor: "white",
+                //   zIndex: 100,
+                // },
+                // drawerPosition: "right",
+                drawerType:drawerTypeForBreakpoint
+            }}
             initialRouteName="Home"
             drawerContent={props => <CustomNavigationDrawer {...props} />}
         >
