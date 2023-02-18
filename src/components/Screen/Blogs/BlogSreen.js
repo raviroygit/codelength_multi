7f408bec4df0bb4ae07d3da62810432c13309b11
jatinderdipole <112872921+jatinderdipole@users.noreply.github.com>
2023-02-18 10:07:36 +0530

    dark mode added to all page

--- src/components/Screen/Blogs/BlogSreen.js
+++ src/components/Screen/Blogs/BlogSreen.js
@@ -2,15 +2,18 @@ import * as React from 'react';
 import { View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import { withTheme } from 'react-native-paper';
+import ScreenBackgroundColor from '../../../utils/ScreenBackground/ScreenBackgroundColor';
 
-const Blogcreen = ({theme,...navProps}) => {
+const Blogcreen = ({ theme, ...navProps }) => {
     const { state, navigation } = navProps;
 
     return (
-        <View>
-            <Button mode='contained' onPress={navigation.goBack}> Blog </Button>
-            <Text style={{fontSize:50}}>Hello Vlogger </Text>
-        </View>
+        <ScreenBackgroundColor>
+            <View>
+                <Button mode='contained' onPress={navigation.goBack}> Blog </Button>
+                <Text style={{ fontSize: 50 }}>Hello Vlogger </Text>
+            </View>
+        </ScreenBackgroundColor>
     );
 };
 
